19eee0230058214bf4c54d0a60daf8f99162ae9c
NayimaUrooj <100697160+NayimaUrooj@users.noreply.github.com>
2022-03-27 16:18:20 +0500

    Update Water.java

--- src/resource/Water.java
+++ src/resource/Water.java
@@ -5,7 +5,7 @@ public class Water extends Resource{ //R10
 	
 	private String type = "water";
 
-	//gets an Asteroid as a parameter and removes water resource from it
+	//gets an Asteroid as a parameter and removes water resource from it, R10
 	@Override
 	public void exposed(Asteroid a)
 	{
